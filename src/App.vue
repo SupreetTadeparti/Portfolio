b37ecb107c0ff9d3902b78b3b2f4e83853b58dc7
Supreet T <supsrasri@gmail.com>
2025-07-31 14:19:36 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Headline from "./components/Headline.vue";
+import ProfilePhoto from "./components/ProfilePhoto.vue";
+</script>
+
+<template>
+  <main>
+    <div class="left">
+      <Headline />
+    </div>
+    <div class="right">
+      <!-- <ProfilePhoto :layer="5" />
+      <ProfilePhoto :layer="4" />
+      <ProfilePhoto :layer="3" /> -->
+      <ProfilePhoto :layer="1" />
+      <!-- <ProfilePhoto :layer="1" /> -->
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  height: 100%;
+  /* background-image: url("./assets/bg.svg"); */
+  background-color: hsl(var(--black));
+  background-size: cover;
+  padding: 2em;
+}
+
+.left {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Headline.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="name">
+    <p>Hi there, my name is</p>
+    <h1 class="highlight primary">Supreet Tadeparti</h1>
+    <p>I am a <span class="highlight secondary">software developer.</span></p>
+  </div>
+</template>
+
+<style scoped>
+* {
+  --text-clr: var(--white);
+  color: hsl(var(--text-clr));
+}
+
+h1 {
+  font-size: 4.5em;
+}
+
+p {
+  font-size: 1.5em;
+  font-family: Funnel Sans;
+}
+
+.primary.highlight {
+  --text-clr: var(--primary);
+}
+
+.secondary.highlight {
+  --text-clr: var(--secondary);
+}
+
+.highlight {
+  text-shadow: 0 0 5px hsl(var(--text-clr));
+}
+</style>
